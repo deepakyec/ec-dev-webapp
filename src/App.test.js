5c8e79166e1a96134b20d36ea754b13c850a3bab
deepakyec <deepak.yadav@experiencecommerce.com>
2017-11-10 16:21:49 +0530

    testing unit...

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import TestRenderer from 'react-test-renderer'; 
 import App from './App';
+
+describe('app', () => {
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
+});
+it('renders correctly for App', () => {
+	const testRenderer = TestRenderer.create(<App />);
+	const testInstance = testRenderer.root;
+	expect(testInstance.findByProps({className: "App-title"}).children).toEqual(['Welcome to EC Dev Web App...']);
+});
+
 });
